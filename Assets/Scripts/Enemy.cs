9e1f54b889ee6584c20a6a2ac8c0ca03b8f400a9
Jose <78896687+jv7924@users.noreply.github.com>
2023-07-13 16:26:04 -0700

    ugh

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public Transform player;
+    [Range(.01f, .05f)]
+    public float distCast = .05f;
+    [Range(0f, 180f)]
+    public float angle = 0f;
+
+    public Collider2D col;
+    private bool canWalk = true;
+    // public Transform player;
     public float moveSpeed = 5f;
     private Rigidbody2D rb;
-    private Vector2 direction;
+    // private Vector2 direction;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +24,13 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 dir = player.position - transform.position;
-        dir.Normalize();
-        direction = dir;
+        // Vector3 dir = player.position - transform.position;
+        // dir.Normalize();
+        // direction = dir;
+        CanWalk();
+        if (canWalk)
+            rb.velocity = new Vector2(1 * moveSpeed, rb.velocity.y);
+
     }
 
     /// <summary>
@@ -27,12 +38,28 @@ public class Enemy : MonoBehaviour
     /// </summary>
     private void FixedUpdate()
     {
-        FollowPlayer(direction);
+        // FollowPlayer(direction);
     }
 
-    private void FollowPlayer(Vector2 direction)
+    // private void FollowPlayer(Vector2 direction)
+    // {
+    //     rb.velocity = new Vector2(transform.position.x + direction.x * moveSpeed, rb.velocity.y);
+    //     // rb.MovePosition((Vector2)transform.position + direction * moveSpeed * Time.deltaTime);
+    // }
+
+    private void CanWalk()
     {
-        rb.velocity = new Vector2(transform.position.x + direction.x * moveSpeed, rb.velocity.y);
-        // rb.MovePosition((Vector2)transform.position + direction * moveSpeed * Time.deltaTime);
+
+        RaycastHit2D raycastHit = Physics2D.BoxCast(col.bounds.center, col.bounds.size - new Vector3(0, .1f, 0), angle, Vector2.right, distCast);
+
+
+        if (raycastHit.collider != null)
+        {
+            canWalk = false;
+        }
+        else 
+        {
+            canWalk = true;
+        }
     }
 }
