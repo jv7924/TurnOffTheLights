17f6de4b1d7b995c8d8e54b7ec47912bf6265ef5
Jose <78896687+jv7924@users.noreply.github.com>
2023-07-15 22:56:51 -0700

    fix enemy ai

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [Range(.01f, .05f)]
-    public float distCast = .05f;
+    public float distCast = .1f;
     [Range(0f, 180f)]
     public float angle = 0f;
 
+    public LayerMask layerMask;
+
     public Collider2D col;
-    private bool canWalk = true;
+    // private bool canWalk = true;
     // public Transform player;
     public float moveSpeed = 5f;
     private Rigidbody2D rb;
+
+    public float direction = 1;
+
     // private Vector2 direction;
     // Start is called before the first frame update
     void Start()
@@ -27,9 +32,7 @@ public class Enemy : MonoBehaviour
         // Vector3 dir = player.position - transform.position;
         // dir.Normalize();
         // direction = dir;
-        CanWalk();
-        if (canWalk)
-            rb.velocity = new Vector2(1 * moveSpeed, rb.velocity.y);
+        
 
     }
 
@@ -39,6 +42,9 @@ public class Enemy : MonoBehaviour
     private void FixedUpdate()
     {
         // FollowPlayer(direction);
+        CanWalk();
+        // if (canWalk)
+        rb.velocity = new Vector2(direction * moveSpeed, rb.velocity.y);
     }
 
     // private void FollowPlayer(Vector2 direction)
@@ -49,24 +55,24 @@ public class Enemy : MonoBehaviour
 
     private void CanWalk()
     {
-        RaycastHit2D raycastHit = Physics2D.BoxCast(col.bounds.center, col.bounds.size - new Vector3(0, .1f, 0), angle, Vector2.right, distCast);
+        RaycastHit2D raycastHit = Physics2D.BoxCast(col.bounds.center, col.bounds.size - new Vector3(0, .1f, 0), angle, new Vector2(0f, direction), distCast, layerMask);
 
         Color rayColor;
-        if (raycastHit.collider != null)
+        if (raycastHit.collider == null)
         {
             rayColor = Color.green;
-            canWalk = true;
+            // canWalk = true;
         }
         else
         {
             rayColor = Color.red;
-            canWalk = false;
+            // canWalk = false;
+            direction *= -1;
         }
 
         Debug.DrawRay(col.bounds.center + new Vector3(0f, col.bounds.extents.y - .05f, 0f), Vector3.right * (col.bounds.extents.x + distCast), rayColor);
         Debug.DrawRay(col.bounds.center - new Vector3(0f, col.bounds.extents.y - .05f, 0f), Vector3.right * (col.bounds.extents.x + distCast), rayColor);
         Debug.DrawRay(col.bounds.center + new Vector3(col.bounds.extents.x + distCast, 0f, 0f), Vector3.down * col.bounds.extents.y + new Vector3(0f, .05f, 0f), rayColor);
         Debug.DrawRay(col.bounds.center + new Vector3(col.bounds.extents.x + distCast, 0f, 0f), Vector3.up * col.bounds.extents.y - new Vector3(0f, .05f, 0f), rayColor);
-
     }
 }
