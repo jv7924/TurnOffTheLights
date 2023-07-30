c73922f655e3b0ddcea4a84e23a2c4e7e7ff31b5
Jose <78896687+jv7924@users.noreply.github.com>
2023-07-30 13:33:43 -0700

    Platform move works

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -9,18 +9,55 @@ public class MovingPlatform : MonoBehaviour
     public Transform endPos;
     public Rigidbody2D platRB;
 
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    private void Awake()
+    {
+        gameObject.transform.position = startPos.position;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.transform.position = startPos.position;
+        // StartCoroutine(MovePlatform(startPos.position, endPos.position));
+        StartCoroutine(LerpPosition(endPos.position, speed));
+
         // StartCoroutine(Plat(startPos, endPos, speed));
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    private void FixedUpdate()
     {
 
-        // ToStart(startPos, endPos, speed);
+    }
+
+    private IEnumerator MovePlatform(Vector2 startPosition, Vector2 endPosition)
+    {
+        float i = 0;
+        float rate = 1f / speed;
+
+        while (i < 1f)
+        {
+            i += Time.deltaTime * rate;
+            platRB.MovePosition(Vector2.Lerp(startPosition, endPosition, i));
+
+            yield return null;
+        }
+    }
+
+    IEnumerator LerpPosition(Vector2 targetPosition, float duration)
+    {
+        float time = 0;
+        while (time < duration)
+        {
+            platRB.MovePosition(Vector2.Lerp(startPos.position, targetPosition, time / duration));
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetPosition;
     }
 
     // private IEnumerator Plat(Transform start, Transform end, float speed)
